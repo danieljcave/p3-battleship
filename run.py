4aaed276bc000a9df42dad3e0a19fe9208db230b
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-08-15 23:03:37 +0100

    add ship location for entering a row number and column letter. if not valid shows user error message and creates a  loop for user to enter a valid row and column

--- run.py
+++ run.py
@@ -26,7 +26,15 @@ def create_ships(board):
         board[ship_row][ship_column] = 'X'
 
 def get_ship_location():
-    pass
+    row = input('Please enter a ship row 1-8')
+    while row not in '12345678':
+        print('Please enter a valid row')
+        row = input('Please enter a ship row 1-8')
+    column  = input('Please enter a ship column A-H').upper()
+    while column not in 'ABCDEFGH':
+        print('Please enter a valid column')
+        column = input('Please enter a ship column A-H').upper()
+    return int(row) - 1, letters_to_numbers[column]
 
 def count_ships_hit():
     pass

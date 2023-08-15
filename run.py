bcd11a853f7aef8abbf61a7779ed1b680a71b517
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-08-15 21:43:41 +0100

    add a hiddend and guess board with range of 8 so 0 through to 7

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+def board():
+    pass

287adeca6b9d4bf40b590307bb5f9aab0a1a55af
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-08-15 22:18:03 +0100

    add print board information for the row nuber that loops to add 1 row

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
+from random import randint
 
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
-def board():
-    pass
+def print_board(board):
+    print('     A B C D E F G H')
+    print('     ---------------')
+    row_numb = 1
+    for row in board:
+        print("%d¦%s¦" % (row_numb, "¦".join(row)))
+        row_numb += 1
 
 def create_ships():
-    pass
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0,7), randint(0,7)
 
 def get_ship_location():
     pass

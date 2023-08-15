48a8d01cce60532db4bcf97a4f1252e2b988548a
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-08-15 22:05:13 +0100

    add def paramaters for game, board creating the ships, the location of the ships and when the ships are hit

--- run.py
+++ run.py
@@ -2,5 +2,20 @@
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
 def board():
     pass
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_ships_hit():
+    pass
+
+create_ships()
+turns = 10
+#while turns > 0:

b61baf2f2e4b4a376b922ab0bc53a283ec15aa3c
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-08-16 09:55:26 +0100

    add information key to the page for hits, misses and avalible spaces to guess the battleship location

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#Legend
+# Battleship game keys for hit, miss and avalible spaces to guess.
 # 'X' = Battleship hit
 # ' ' = for avalible spaces
 # 'O' = for a missed shot
@@ -8,7 +8,16 @@ from random import randint
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
-letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+    }
 
 def print_board(board):
     print('  A B C D E F G H')
